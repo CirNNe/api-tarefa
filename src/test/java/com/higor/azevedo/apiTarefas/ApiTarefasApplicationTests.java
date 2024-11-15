d6d8a46b398462090f54846c06458fb817efcd89
Higor Cirne <cirnne1@outlook.com>
2024-11-15 13:16:24 -0300

    feat: start do projeto

---
+++ src/test/java/com/higor/azevedo/apiTarefas/ApiTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higor.azevedo.apiTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
